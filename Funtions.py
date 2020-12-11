276992f796921f87de6b67350cc4aa876b59c9b1
sandeeppy-ds <75848577+sandeeppy-ds@users.noreply.github.com>
2020-12-11 20:43:42 +0530

    day 1 of python
    Datatypes and functions

---
+++ Funtions.py
@@ -0,0 +1,75 @@
+'''   ******Functions****     '''
+
+#Funtions are mainly used to write the logic
+# Functions can be called/acessed
+# Functions can be defined with any names ( remember naming convention while defining a function is simillar to the variable)
+#Functions will always have paranthesis followed by function name and is used to define the parameters
+#Indentation is very important while creating a function. we can write n no.of lines of code/logic inside a function.
+# Fnctions needs to be called in order to expect the output of a function.
+
+
+
+#using global variables as arguments to the function ( Note that if none of the parameters are defined for a function by default it will look for the global variables)
+
+x = 2
+y = 4
+
+def calc():
+	print (x + 2)
+	print (y + 4)
+
+calc()
+
+
+#Defining a function with arguments
+#Here x,y are positional arguments. when ever we pass the parameters by default it will be taken by using the position.
+
+def calc(x,y):
+	print (x + 2)
+	print (y + 4)
+
+calc(3,5)
+
+
+#Defining a function with arguments and changing the positions
+#positional argument always follows keyword argument
+def calc(x,y):
+	print (x + 3)
+	print (y + 2)
+
+calc(y = 2, x = 4)
+
+calc( 4 , y = 3)
+
+#defing a variable inside a function with * as pre deccissor makes it as a tuple
+	#Note that we an only use * for one variable and we cant use for multiple  variables
+def calc(x,*y):
+	print (x)
+	print (y)
+
+calc(1,4,8,9)
+
+calc(2,2,4,3 )
+
+
+#defining a variable inside a function with ** as pre-decissor makes it a Dictionary
+
+def calc(x,**y):
+	print(x)
+	print(y)
+
+calc(4)
+
+
+#We can define a function within a function and is valid only on certain below conditons
+   #we need to call the location function inside a global function in order to access it globally.
+
+def project_y(x):
+	print(x)
+	def project_v(a):
+		print ( a + 2)
+	project_v(4)
+
+
+
+project_y(7)
---
+++ datatypes.py
@@ -0,0 +1,57 @@
+'''  learning about datatypes '''
+
+''' python is dynamically typed programing language '''
+
+# where as Java is staticstially typed programing language '''
+
+
+''' Data Types '''
+
+# int : every non decimal value is an integer
+# Float : contains Decimal valuess
+#String : Its a character or word
+#Boolean : True or False
+#None : Nothing or Null
+
+''' Variables '''
+
+ #variables are used to store the data
+ #variables are defined in two ways 
+   #type of variable means, what type of data type it is..
+   #Kind of variable means, what kind of variable i.e, LOCAL or GLOBAL
+   		#Local variable : variable which is defined inside a particular function or defined with indentation and it only belongs locally
+   		#Global variable : variable which is defined to the extreme Left wall or etreme left of a console. And it can be accessed
+   		                   # globally and locally.
+   		# There are Certain points while naming a variable.
+   				#Its always reommended that using Lower ase letters or words while creating a variable
+   				#Using Underscore(_) is the best option if we have to create a variable using multiple words (ex : data_type = 'int')
+
+x = 10
+print (x)
+print (type(x))
+
+y = 91.9
+print (y)
+print (type(y))
+
+a = 'Sandeep'
+print (a)
+print(type(a))
+
+a = "tony"
+print (a)
+
+a = ''" star "''
+print (a)
+
+b = True
+print(b)
+print(type(b))
+
+
+d = None
+print(d)
+print(type(d))
+
+
+
